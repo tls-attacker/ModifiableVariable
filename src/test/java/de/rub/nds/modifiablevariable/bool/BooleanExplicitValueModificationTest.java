5dde93761aa772ecf4131f8f43dc41374e3061a8
Albtraumbaum <118673879+Albtraumbaum@users.noreply.github.com>
2022-12-06 00:23:08 +0100

    Implement BooleanExplicitValueModificationTest #HSFDPMUW  (#102)
    * Unit Tests BooleanExplicitValueModificationTest
    
    * Unit Tests BooleanExplicitValueModificationTest

--- src/test/java/de/rub/nds/modifiablevariable/bool/BooleanExplicitValueModificationTest.java
+++ src/test/java/de/rub/nds/modifiablevariable/bool/BooleanExplicitValueModificationTest.java
@@ -1,19 +1,18 @@
-/**
+/*
  * ModifiableVariable - A Variable Concept for Runtime Modifications
  *
- * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
  *
- * Licensed under Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0.txt
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
  */
-
 package de.rub.nds.modifiablevariable.bool;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class BooleanExplicitValueModificationTest {
@@ -21,73 +20,59 @@ public class BooleanExplicitValueModificationTest {
     private BooleanExplicitValueModification b1;
     private BooleanExplicitValueModification b2;
     private BooleanExplicitValueModification b3;
-    private Integer integer1;
 
     @BeforeEach
     public void setUp() {
         b1 = new BooleanExplicitValueModification(true);
         b2 = new BooleanExplicitValueModification(false);
         b3 = new BooleanExplicitValueModification(true);
-        integer1 = 1;
     }
 
-    /**
-     * Test of modifyImplementationHook method, of class BooleanExplicitValueModification.
-     */
-    @Disabled("Not yet implemented")
+    /** Test of modifyImplementationHook method, of class BooleanExplicitValueModification. */
     @Test
     public void testModifyImplementationHook() {
+        assertTrue(b1.modifyImplementationHook(true));
+        assertTrue(b1.modifyImplementationHook(false));
     }
 
-    /**
-     * Test of isExplicitValue method, of class BooleanExplicitValueModification.
-     */
-    @Disabled("Not yet implemented")
+    /** Test of getExplicitValue method, of class BooleanExplicitValueModification. */
     @Test
-    public void testIsExplicitValue() {
+    public void testGetExplicitValue() {
+        assertTrue(b1.getExplicitValue());
+        assertFalse(b2.getExplicitValue());
     }
 
-    /**
-     * Test of setExplicitValue method, of class BooleanExplicitValueModification.
-     */
-    @Disabled("Not yet implemented")
+    /** Test of setExplicitValue method, of class BooleanExplicitValueModification. */
     @Test
     public void testSetExplicitValue() {
+        b2.setExplicitValue(true);
+        assertTrue(b2.getExplicitValue());
     }
 
-    /**
-     * Test of getModifiedCopy method, of class BooleanExplicitValueModification.
-     */
-    @Disabled("Not yet implemented")
+    /** Test of getModifiedCopy method, of class BooleanExplicitValueModification. */
     @Test
     public void testGetModifiedCopy() {
+        assertNotEquals(b1, b1.getModifiedCopy());
     }
 
-    /**
-     * Test of hashCode method, of class BooleanExplicitValueModification.
-     */
-    @Disabled("Not yet implemented")
+    /** Test of hashCode method, of class BooleanExplicitValueModification. */
     @Test
     public void testHashCode() {
+        assertEquals(b3.hashCode(), b1.hashCode());
     }
 
-    /**
-     * Test of equals method, of class BooleanExplicitValueModification.
-     */
-    @Disabled("Not yet implemented")
+    /** Test of equals method, of class BooleanExplicitValueModification. */
     @Test
     public void testEquals() {
+        assertEquals(b1, b3);
+        assertNotEquals(b1, b2);
     }
 
-    /**
-     * Test of toString method, of class BooleanExplicitValueModification.
-     */
+    /** Test of toString method, of class BooleanExplicitValueModification. */
     @Test
     public void testToString() {
         assertNotEquals(b1, b2);
-        assertNotEquals(b1, integer1);
         assertEquals(b1, b1);
         assertEquals(b1, b3);
     }
-
 }
