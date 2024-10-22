58266a3d46634980dc5db545624e1003f05b94ef
mmaehren <32199075+mmaehren@users.noreply.github.com>
2024-10-22 11:00:12 +0200

    Added suppressing adapters to leave default values out of XML

---
+++ src/main/java/de/rub/nds/modifiablevariable/util/SuppressingTrueBooleanAdapter.java
@@ -0,0 +1,15 @@
+/*
+ * ModifiableVariable - A Variable Concept for Runtime Modifications
+ *
+ * Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.modifiablevariable.util;
+
+public class SuppressingTrueBooleanAdapter extends SuppressingBooleanAdapter {
+    @Override
+    public Boolean getValueToSuppress() {
+        return Boolean.TRUE;
+    }
+}
