de801a6c2d6a10d8b4895a5521a837286f24b946
Robert Merget <robert.merget@tii.ae>
2025-03-19 14:23:39 +0400

    formatted and updated readme

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ If you want to use this project as a dependency, you do not have to compile it y
 <dependency>
     <groupId>de.rub.nds</groupId>
     <artifactId>modifiable-variable</artifactId>
-    <version>5.0.0</version>
+    <version>5.0.1</version>
 </dependency>
 ```
 
--- src/test/java/de/rub/nds/modifiablevariable/util/RandomHelperTest.java
+++ src/test/java/de/rub/nds/modifiablevariable/util/RandomHelperTest.java
@@ -9,7 +9,6 @@ package de.rub.nds.modifiablevariable.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Random;
